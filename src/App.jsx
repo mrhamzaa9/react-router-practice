085534cacc4b3f555bb6ca68af5fc59a0c56d167
mrhamzaa9 <hamzathor470@gmail.com>
2025-11-06 18:00:37 +0500

    new way routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React ,{ useState } from 'react'
-import Header from './components/header/Header'
-import Home from './components/Home'
-import About from './components/About'
-import Footer from './components/footer/Footer'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Services from './components/services.jsx'
 
 
 function App() {
-
-
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='service' element={<Services />} />
+      </Route>
+    </Routes>
   )
 }
 
 export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from "react-router-dom";
-import Home from './components/Home.jsx';
-import About from './components/About.jsx';
-import Services from './components/services.jsx';
-import Layout from './layout.jsx';
-const router = createBrowserRouter(
-  createRoutesFromElements(
-        <Route path='/' element={<Layout />}>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-        <Route path='/service' element={< Services/>} />
-    </Route>
-
-  )
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
+    <BrowserRouter>
       <App />
-    </RouterProvider>
+    </BrowserRouter>
   </StrictMode>
 )
